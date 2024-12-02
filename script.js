0ecca2d731dff1845bf42efe882e0f926e539c7f
kiripolsky <kiripolskypavel@gmail.com>
2024-12-02 22:28:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ window.onload = function() {
         favicon.href = "images/favicon2.png";
     }
 };
+
+function changeText() {
+    document.getElementById("text").innerHTML = "kiripolskypavel@gmail.com";
+    setInterval(copyToClipboard, 1000);
+    setInterval(changeTextBack, 1000);
+}
+
+function changeTextBack() {
+    document.getElementById("text").innerHTML = "Email";
+}
+
+function copyToClipboard() {
+    navigator.clipboard.writeText("kiripolskypavel@gmail.com").then(function() {
+        alert("Email byl zkopírován do schránky!");
+    }).catch(function(error) {
+        alert("Došlo k chybě při kopírování: " + error);
+    });
+}
